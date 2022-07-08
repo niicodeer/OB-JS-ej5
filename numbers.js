53cd8660a0794c142d27ac5827b435c71485ea2e
niicodeer <nicoradin.job@gmail.com>
2022-07-08 16:08:16 -0300

    number

---
+++ numbers.js
@@ -0,0 +1,26 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let alturacm = 172 //cm
+console.log(alturacm);
+
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let alturam = 1.72  //m
+console.log(alturam);
+
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesokg = 73.8//kg
+console.log(pesokg);
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRounded = Math.round(alturam);
+console.log(alturaRounded);
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoFloor = Math.floor(pesokg);
+console.log(pesoFloor);
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+
+
+
+
